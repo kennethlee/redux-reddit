5c8c926f950e5f053e43f3fb512c10d89d6de12a
Thomas Lo <thomasjinlo@gmail.com>
2016-07-29 15:29:28 -0700

    Refactor app into dumb component, create subreddit container

--- src/components/app.js
+++ src/components/app.js
@@ -1,43 +1,10 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import React from 'react';
+import SubredditList from '../containers/subreddits/container';
 
-import { fetchPopularSubs } from '../actions';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidMount() {
-    this.props.fetchPopularSubs()
-  }
-
-  renderSubreddits() {
-    console.log("IN RENDER ", this.props.subreddits)
-    return this.props.subreddits.map((sub) => {
-      return <div>
-              <h3>{sub.display_name}</h3>
-            </div>
-    });
-  }
-
-  render() {
-    return (<div>
-      {this.renderSubreddits()}
-    </div>);
-  }
+export default () => {
+  return (
+    <div>
+      <SubredditList />
+    </div>
+  );
 }
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchPopularSubs: fetchPopularSubs }, dispatch);
-}
-
-function mapStateToProps(state) {
-  return {
-    subreddits: state.subreddits,
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
